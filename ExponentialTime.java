91d86b98e3b83baadeb356382bee1fb77e917426
VarikutiMamatha <varikutimamathareddy@gmail.com>
2025-06-12 19:54:37 +0530

    Add files via upload

--- ExponentialTime.java
+++ ExponentialTime.java
@@ -8,7 +8,7 @@ public class ExponentialTime {
 		return fibonacci(n-1)+fibonacci(n-2);	
 	}
 	public static void main(String [] args) {
-		int n=5;
+		int n=6;
 		System.out.println("fibonacci of"+n+"is:"+fibonacci(n));
 		
 	}  
--- Fibanocciex.java
+++ Fibanocciex.java
@@ -1,18 +1,16 @@
 package xyz;
 
 public class Fibanocciex {
-
-	public static void main(String[] args) {
-		
-		        int n = 10; 
-		        int a = 0, b = 1;
-
-		        System.out.println("Fibonacci Series up to " + n + " terms:");
-		        for (int i = 0; i < n; i++) {
-		            System.out.print(a + " ");
-		            int next = a + b;
-		            a = b;
-		            b = next;
-		        }
-		    }
+	public static int fibonacci(int n) {
+		if(n<=1) {
+			return n;
 		}
+		return fibonacci(n-1)+fibonacci(n-2);	
+	}
+	public static void main(String [] args) {
+		int n=6;
+		System.out.println("fibonacci of"+n+"is:"+fibonacci(n));
+		
+	}  
+
+}
